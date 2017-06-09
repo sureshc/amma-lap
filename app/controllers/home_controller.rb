19e26b2c25c7fcc119f448f65f8015ea1ba33439
Suresh Chanmugam <me@suresh.com>
2017-06-09 14:05:06 -0700

    Add logic to destroy associated child picks when a child is destroyed.  Also fix defect in Child View when a child has never been picked (such as when it was just created).

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def pick
     @child = Child.all.sample
-    @child_pick = ChildPick.new({ child: @child, picked: Time.now })
+    @child_pick = @child.child_picks.create(picked: Time.now)
     @child_pick.save
   end
 end
--- app/models/child.rb
+++ app/models/child.rb
@@ -1,5 +1,5 @@
 class Child < ApplicationRecord
-  has_many :child_picks
+  has_many :child_picks, dependent: :destroy
   validates :name, presence: true, length: { maximum: 255 },
             uniqueness: true
 end
