325f6ea3223ea044acfde8b1cdfe7ce85497b649
Suresh Chanmugam <me@suresh.com>
2017-06-09 10:37:08 -0700

    Add ability to pick a random child (and save that pick in the history) - Issue #3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def pick
+    @child = Child.all.sample
+    @child_pick = ChildPick.new({ child: @child, picked: Time.now })
+    @child_pick.save
   end
 end
